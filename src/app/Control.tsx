3947ac8f4136874e9206ed0373aacb91570e8e4c
tnemnorivnelee <tnemnorivnelee@gmail.com>
2025-03-18 22:56:58 +0900

    feat: 글 삭제

--- src/app/Control.tsx
+++ src/app/Control.tsx
@@ -1,10 +1,11 @@
 "use client";
 
 import Link from "next/link";
-import {useParams} from "next/navigation";
+import {useParams, useRouter} from "next/navigation";
 
 export default function Control() {
     const params = useParams();
+    const router = useRouter();
     const id = params.id;
 
     return (
@@ -12,7 +13,14 @@ export default function Control() {
             <li><Link href="/create">Create</Link></li>
             {id ? <>
                 <li><Link href={"/update/" + id}>Update</Link></li>
-                <li><input type="button" value="delete"/></li>
+                <li><input type="button" value="delete" onClick={() => {
+                    const options = {method: "DELETE"};
+
+                    fetch(`http://localhost:9999/topics/${id}/`, options).then(resp => resp.json()).then(result => {
+                        router.push("/");
+                        router.refresh();
+                    });
+                }}/></li>
                 </>
                 : null}
         </ul>
